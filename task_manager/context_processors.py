d36d49711a27a2a654d0014cc75d56e51e6e55c7
ribeyra <ribeyra@gmail.com>
2024-06-15 21:22:49 +0300

    update <html lang=...

--- task_manager/context_processors.py
+++
@@ -1,7 +0,0 @@
-from django.utils import translation
-
-
-def current_language(request):
-    return {
-        'LANGUAGE_CODE': translation.get_language(),
-    }
--- task_manager/settings.py
+++ task_manager/settings.py
@@ -76,7 +76,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'task_manager.context_processors.current_language',
             ],
         },
     },
