fa70064f1eda94dadd24c411157a6d800bd307ae
ribeyra <ribeyra@gmail.com>
2024-07-07 21:35:12 +0300

    update mixins

--- task_manager/mixins.py
+++ task_manager/mixins.py
@@ -1,22 +1,21 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseRedirect
 from django.shortcuts import redirect
 from django.utils.translation import gettext as _
 
 
-class LoginRequiredAndUserIsSelfMixin(LoginRequiredMixin):
+class LoginRequiredMixinWithFlash(LoginRequiredMixin):
+    def handle_no_permission(self):
+        messages.add_message(
+            self.request,
+            messages.ERROR,
+            _('You are not authorized! Please log in')
+        )
+        return redirect('login')
+
+
+class UserIsSelfMixin:
     def dispatch(self, request, *args, **kwargs):
-        response = super().dispatch(request, *args, **kwargs)
-
-        if isinstance(response, HttpResponseRedirect):
-            messages.add_message(
-                request,
-                messages.ERROR,
-                _('You are not authorized! Please log in')
-            )
-            return response
-
         user_id = kwargs.get('pk')
         if user_id != request.user.id:
             messages.add_message(
@@ -25,5 +24,4 @@ class LoginRequiredAndUserIsSelfMixin(LoginRequiredMixin):
                 _('You do not have permission to change another user')
             )
             return redirect('users')
-
-        return response
+        return super().dispatch(request, *args, **kwargs)
