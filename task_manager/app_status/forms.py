98728f8aa92edb284cdf4ad01e4638117e823005
ribeyra <ribeyra@gmail.com>
2024-07-09 20:22:38 +0300

    update forms

--- task_manager/app_status/forms.py
+++ task_manager/app_status/forms.py
@@ -4,9 +4,9 @@ from .models import Status
 
 
 class StatusCreationForm(forms.ModelForm):
-    status_name = _("Status name")
+    status_name = _("Name")
     name = forms.CharField(label=status_name, max_length=40, required=True)
-    status_description = _("Status description")
+    status_description = _("Description")
     description = forms.CharField(
         label=status_description,
         max_length=255,
--- task_manager/app_tag/forms.py
+++ task_manager/app_tag/forms.py
@@ -4,9 +4,9 @@ from .models import Tag
 
 
 class TagCreationForm(forms.ModelForm):
-    status_name = _("Tag name")
+    status_name = _("Name")
     name = forms.CharField(label=status_name, max_length=40, required=True)
-    status_description = _("Tag description")
+    status_description = _("Description")
     description = forms.CharField(
         label=status_description,
         max_length=255,
