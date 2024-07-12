85271a5a54300aafc9117dc9c9833a337dd40216
ribeyra <ribeyra@gmail.com>
2024-07-12 21:38:47 +0300

    lint errors fix

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -166,16 +166,22 @@ ROLLBAR = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# This setting informs Django of the URI path from which your static files will be served to users
-# Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
+# This setting informs Django of the URI path from which your static files will
+# be served to users
+# Here, they well be accessible at your-domain.onrender.com/static/... or
+# yourcustomdomain.com/static/...
 STATIC_URL = '/static/'
 
-# This production code might break development mode, so we check whether we're in DEBUG mode
+# This production code might break development mode, so we check whether we're
+# in DEBUG mode
 if not DEBUG:
-    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    # Tell Django to copy static assets into a path called `staticfiles` (this
+    # is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-    # and renames the files with unique names for each version to support long-term caching
+    # Enable the WhiteNoise storage backend, which compresses static files to
+    # reduce disk use
+    # and renames the files with unique names for each version to support
+    # long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
