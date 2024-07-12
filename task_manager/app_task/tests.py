1e3080e85bc9bb3a892c642619ff7bcaf7ff4116
ribeyra <ribeyra@gmail.com>
2024-07-12 17:20:19 +0300

    add filter tests

--- task_manager/app_task/tests.py
+++ task_manager/app_task/tests.py
@@ -100,3 +100,92 @@ class TaskTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'tasks/detail.html')
         self.assertContains(response, 'Test Task')
+
+
+class TaskFilterTests(TestCase):
+    def setUp(self):
+        self.user1 = get_user_model().objects.create_user(
+            username='user1',
+            password='password123'
+        )
+        self.user2 = get_user_model().objects.create_user(
+            username='user2',
+            password='password123'
+        )
+        self.client.login(username='user1', password='password123')
+
+        self.status1 = Status.objects.create(
+            name='Open',
+            description='Open tasks'
+        )
+        self.status2 = Status.objects.create(
+            name='Closed',
+            description='Closed tasks'
+        )
+
+        self.label1 = Label.objects.create(
+            name='Bug',
+            description='Bug related task'
+        )
+        self.label2 = Label.objects.create(
+            name='Feature',
+            description='Feature related task'
+        )
+
+        self.task1 = Task.objects.create(
+            title='Task 1',
+            description='Description 1',
+            status=self.status1,
+            executor=self.user1,
+            author=self.user1
+        )
+        self.task1.label.add(self.label1)
+
+        self.task2 = Task.objects.create(
+            title='Task 2',
+            description='Description 2',
+            status=self.status2,
+            executor=self.user2,
+            author=self.user2
+        )
+        self.task2.label.add(self.label2)
+
+    def test_filter_by_status(self):
+        url = reverse('tasks')
+        response = self.client.get(url, {'status': self.status1.id})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Task 1')
+        self.assertNotContains(response, 'Task 2')
+
+    def test_filter_by_executor(self):
+        url = reverse('tasks')
+        response = self.client.get(url, {'executor': self.user1.id})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Task 1')
+        self.assertNotContains(response, 'Task 2')
+
+    def test_filter_by_label(self):
+        url = reverse('tasks')
+        response = self.client.get(url, {'label': self.label1.id})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Task 1')
+        self.assertNotContains(response, 'Task 2')
+
+    def test_filter_self_tasks(self):
+        url = reverse('tasks')
+        response = self.client.get(url, {'self_tasks': 'on'})
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Task 1')
+        self.assertNotContains(response, 'Task 2')
+
+    def test_filter_combined(self):
+        url = reverse('tasks')
+        response = self.client.get(url, {
+            'status': self.status1.id,
+            'executor': self.user1.id,
+            'label': self.label1.id,
+            'self_tasks': 'on'
+        })
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Task 1')
+        self.assertNotContains(response, 'Task 2')
